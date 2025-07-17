acb805cbc9a41743e30779f7dc63883be22623bc
onurr_k <145716470+onurrrk@users.noreply.github.com>
2025-07-17 22:21:58 +0300

    Update Title.java

--- src/leaderos/web/tr/dailyreward/utils/Title.java
+++ src/leaderos/web/tr/dailyreward/utils/Title.java
@@ -69,6 +69,41 @@ public class Title extends Reflection {
 	      var11.printStackTrace();
 	    }
 	  }
+package leaderos.web.tr.dailyreward.utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import leaderos.web.tr.dailyreward.Main;
+
+public class Title {
+
+	public static void sendTitle(Player player, String title, String subtitle, int fadeIn, int stay, int fadeOut) {
+		if (player == null) return;
+		if (title == null) title = "";
+		if (subtitle == null) subtitle = "";
+
+		final String finalTitle = title;
+		final String finalSubtitle = subtitle;
+
+		if (!Bukkit.isPrimaryThread()) {
+			Bukkit.getScheduler().runTask(Main.instance, () ->
+					sendTitle(player, finalTitle, finalSubtitle, fadeIn, stay, fadeOut)
+			);
+			return;
+		}
+
+		player.sendTitle(
+				Main.color(finalTitle.replace("%player%", player.getName())),
+				Main.color(finalSubtitle.replace("%player%", player.getName())),
+				fadeIn, stay, fadeOut
+		);
+	}
+
+	public static void sendTitleSeconds(Player player, String title, String subtitle, int fadeInSeconds, int staySeconds, int fadeOutSeconds) {
+		sendTitle(player, title, subtitle, fadeInSeconds * 20, staySeconds * 20, fadeOutSeconds * 20);
+	}
+
+}
 
 	  public static void sendPacket(Player player, Object packet) {
 	    try {
